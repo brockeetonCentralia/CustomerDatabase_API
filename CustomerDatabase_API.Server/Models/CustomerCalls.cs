46d1f4c7451bb643bdbe34a3338bd0e7570272af
Broc Keeton <broc.keeton@student.centralia.edu>
2024-02-22 11:47:07 -0800

    Add project files.

---
+++ CustomerDatabase_API.Server/Models/CustomerCalls.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerDatabase_API.Server.Models
+{
+    public class CustomerCalls
+    {
+        public int CallId { get; set; }
+        [Required]
+        public int CallAttempts { get; set; }
+        [Required]
+        public string CallNotes { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ CustomerDatabase_API.Server/Models/CustomerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerDatabase_API.Server.Models
+{
+    public class CustomerInfo
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+    }
+}
